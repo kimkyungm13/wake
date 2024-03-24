86ab99efbac613bdef5b3440b94149c17b2d4f12
kimkyungm13 <kyung_m13@naver.com>
2024-03-25 00:37:56 +0900

    2

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,4 +9,28 @@ $(function () {
             prevEl: ".btn-prev",
         },
     });
+    gsap.to('.sc-rooms .rooms-wrap', {
+        scrollTrigger: {
+            trigger: ".sc-rooms",
+            start: "0% 0%",
+            end: "100% 100%",
+            scrub: 1,
+            markers: true,
+            invalidateOnRefresh: true,
+            onUpdate: function (self) {
+
+                step = Math.round(self.progress * 3);
+
+                $('.sc-rooms').attr('data-step', step + 1)
+
+                // if(self.progress > 0.5){
+                //     $('.asdas').text('')
+                // }
+            }
+        },
+        xPercent: -100,
+        x: function () {
+            return window.innerWidth;
+        }
+    })
 });
